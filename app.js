32c59adbc498ec24922d3bc2345405c7f6265a48
babita <babitabisht2018@gmail.com>
2019-02-18 12:30:10 +0530

    my commit

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 var express=require('express')
 var app=express()
 const session=require('express-session')
+const User =require('./models/User');
 
 var passport=require('passport')
 const GOOGLE_CLIENT_ID='583658863496-88q6jtvqgej170v2dnpljq6pvpllu9gt.apps.googleusercontent.com'
 const GOOGLE_CLIENT_SECRET='waN_1q2VlJ87U1RlIv_V1vGa'
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
+var mongodb=require('mongodb');
+const mongoose=require('mongoose');
 
+
+const url ='mongodb://localhost:27017/FabricUsers' ;
+mongoose.Promise = global.Promise;
+
+mongoose.connect(url).then(()=>{
+  console.log("mongodb connected ...!");
+}).catch(err=>{
+console.log(err);
+})
 
 
 // passport.serializeUser(function(user, done) {
@@ -25,7 +37,7 @@ app.use(session({
 
   }))
 
-app.use(passport.initialize());
+app.use(passport.initialize())
 app.use(passport.session());
 
 app.set('view engine','ejs')
@@ -39,10 +51,30 @@ passport.use(new GoogleStrategy({
     callbackURL: 'http://localhost:3030/auth/google/callback',
     passReqToCallback   : true
   },
-  function(accessToken, refreshToken, profile, cb) {
+
+  function(request,accessToken, refreshToken, profile, done) {
+
+   var user={ firstName:profile.name.givenName,
+   }
+
+   new User({user}).save()
+   .then(user=> done(null,user))
     console.log(profile)
+
+
   }
-));
+));passport.serializeUser( (user,done)=>{
+  console.log("in serialize  of oauth.............")
+  done(null,user.id);
+} );
+
+passport.deserializeUser( (id,done)=>{
+  console.log("in deserialize of oauth.....................")
+  User.findById(id).then(user =>done(null,user));
+  
+} );
+
+
 
 app.get('/auth/google',
 passport.authenticate('google', { scope:["profile"] }));
@@ -52,12 +84,23 @@ passport.authenticate('google', { failureRedirect: '/login' }),
 function(req, res) {
     console.log(res)
 // Successful authentication, redirect home.
-res.redirect('/');
+console.log(
+  
+"hello !"
+);
+
+
+res.redirect('/success');
 });
-app.get('/login',()=>{
+app.get('/login',(req,res)=>{
     res.render('home')
 })
 
+
+app.get('/success',(req,res)=>{
+res.send('Welcome ....................here !')
+
+})
 
 app.get('/',(req,res)=>{
     res.render('home');
